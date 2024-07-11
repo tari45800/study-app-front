8eaaa4d8b8eb1f37967ae390a2204d6dae5f2232
tari45800 <tari45800@gmail.com>
2024-07-11 22:32:41 +0900

    feat: 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        안녕안녕 나는 지수야
+      </div>
+    </>
+  )
+}
+
+export default App
