eb369175ed219b95404370ae0e7733e573cf0f9e
tari45800 <tari45800@gmail.com>
2024-07-15 00:27:33 +0900

    feat: 타입스크립트 이론 끝

--- src/StudyTypeScript.tsx
+++ src/StudyTypeScript.tsx
@@ -1,65 +1,37 @@
 import React from 'react';
 
 const StudyTypeScript = () => {
-  type Score = 'A' | 'B' | 'C' | 'D';
-
   interface User {
     name: string;
     age: number;
-    gender?: string;
-    readonly birthYear: number;
-    [grade: number]: Score;
   }
-
-  let user: User = {
-    name: 'lee',
-    age: 27,
-    birthYear: 1998,
-    1: 'A',
-    2: 'B',
-  };
-
-  interface Add {
-    (num1: number, num2: number): number;
-  }
-
-  const add: Add = (x, y) => {
-    return x + y;
-  };
 
   interface Car {
-    color: string;
-    wheels: number;
-    start(): void;
-  }
-
-  class Bmw implements Car {
-    color;
-    wheels = 4;
-
-    constructor(c: string) {
-      this.color = c;
-    }
-
-    start() {
-      console.log('go');
-    }
-  }
-
-  const b = new Bmw('green');
-
-  interface Benz extends Car {
-    door: number;
-    stop(): void;
-  }
-
-  interface Toy {
     name: string;
+    color: string;
   }
 
-  interface ToyCar extends Car, Toy {
+  interface Book {
     price: number;
   }
+
+  const user: User = { name: 'a', age: 10 };
+  const car: Car = { name: 'bmw', color: 'red' };
+  const book: Book = { price: 3000 };
+
+  // 제네릭 함수에서 타입 매개변수 T를 사용하고 있으며,
+  // T가 어떤 타입인지 명확히 지정하지 않아서
+  // data 객체에 name 속성이 있는지 타입스크립트가 알 수 없기 때문
+  // 이를 해결하기 위해 T가 name 속성을 가진 객체임을 명시적으로 지정해야 한다.
+  // 코드 설명 : 어떠한 T타입의 데이터가 올 것인데 이는 name이라는 속성을 확장한 형태이다
+  // 다양한 모습의 데이터가 올 수 있겠지만 항상 name:string을 가지고 있어야 한다.
+  const showName = <T extends { name: string }>(data: T) => {
+    return data.name;
+  };
+
+  showName(user);
+  showName(car);
+  // showName(book); 애러 발생
 
   return (
     <>
