dedbc56fe6710bfecc62fb8ccc8639a4a9a1e39c
tari45800 <tari45800@gmail.com>
2024-07-14 20:23:57 +0900

    feat: 프리티어 설정, 타입스크립트 학습

--- src/StudyTypeScript.tsx
+++ src/StudyTypeScript.tsx
@@ -1,6 +1,66 @@
-import React from "react";
+import React from 'react';
 
 const StudyTypeScript = () => {
+  type Score = 'A' | 'B' | 'C' | 'D';
+
+  interface User {
+    name: string;
+    age: number;
+    gender?: string;
+    readonly birthYear: number;
+    [grade: number]: Score;
+  }
+
+  let user: User = {
+    name: 'lee',
+    age: 27,
+    birthYear: 1998,
+    1: 'A',
+    2: 'B',
+  };
+
+  interface Add {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add = (x, y) => {
+    return x + y;
+  };
+
+  interface Car {
+    color: string;
+    wheels: number;
+    start(): void;
+  }
+
+  class Bmw implements Car {
+    color;
+    wheels = 4;
+
+    constructor(c: string) {
+      this.color = c;
+    }
+
+    start() {
+      console.log('go');
+    }
+  }
+
+  const b = new Bmw('green');
+
+  interface Benz extends Car {
+    door: number;
+    stop(): void;
+  }
+
+  interface Toy {
+    name: string;
+  }
+
+  interface ToyCar extends Car, Toy {
+    price: number;
+  }
+
   return (
     <>
       <div>안녕안녕 나는 지수야!</div>
