dd6a6cc68ad4c854fcd85fe4190ada83dc520047
tari45800@gmail.com <tari45800@gmail.com>
2024-08-15 12:14:40 +0900

    feat:

--- src/app/layouts/baseLayout.tsx
+++ src/app/layouts/baseLayout.tsx
@@ -3,13 +3,26 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import { LayoutHeader } from '../../widget/LayoutHeader';
+import { BackGround } from '../../shared/ui';
+import { Timer } from '../../entity/Timer';
 
 const BaseLayoutContainer = styled.div`
   background-color: var(--background-color);
   min-height: 100vh;
+  position: relative;
 
   .faAngleRight {
     color: var(--light-text-color);
+  }
+
+  .absoluteFeatureBox {
+    position: fixed;
+    bottom: var(--spacing-small);
+    left: var(--spacing-small);
+  }
+
+  @media (max-height: 45rem) {
+    background-color: red;
   }
 `;
 
@@ -18,5 +31,10 @@ export const baseLayout = (
     {/* <PageTransition /> */}
     <LayoutHeader />
     <Outlet />
+    <div className="absoluteFeatureBox">
+      <BackGround>
+        <Timer />
+      </BackGround>
+    </div>
   </BaseLayoutContainer>
 );
--- src/entity/Timer/ui/Timer.tsx
+++ src/entity/Timer/ui/Timer.tsx
@@ -8,7 +8,7 @@ export const Timer = () => {
   useEffect(() => {
     startTimer(); // 컴포넌트 마운트 시 타이머 자동 시작
 
-    return () => stopTimer(); // 컴포넌트 언마운트 시 타이머 정리
+    // return () => stopTimer(); // 컴포넌트 언마운트 시 타이머 정리
   }, []);
 
   const formatTime = (seconds: number): string => {
@@ -27,7 +27,7 @@ export const Timer = () => {
 
   return (
     <TimerContainer>
-      <div>{formatTime(seconds)} 공부 중...</div>
+      <div>{formatTime(seconds)}</div>
     </TimerContainer>
   );
 };
